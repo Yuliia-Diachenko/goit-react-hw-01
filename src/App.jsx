6885da7c523fe9252c787a5242ea762efee5153c
Yuliia Diachenko <zirkanew82@gmail.com>
2024-04-25 10:47:06 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Profile from './components/Profile/Profile';
+import Profile from './components/profile/Profile';
 import userData from './userData.json';
 import FriendList from './components/FriendList/FriendList';
 import friends from './friends.json';
