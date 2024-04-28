36e573f586ef3bb3e18ad6429757b4c5a0e2e25c
Yuliia Diachenko <zirkanew82@gmail.com>
2024-04-28 10:42:41 +0300

    fix-deploy-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Profile from './components/profile/Profile';
+import Profile from './components/Profile/Profile';
 import userData from './userData.json';
 import FriendList from './components/FriendList/FriendList';
 import friends from './friends.json';
@@ -8,11 +8,13 @@ import css from './App.module.css';
 
 function App() { 
   return (
+
     <div className={css.container}>
     <Profile userData = {userData}/>
     <FriendList friends={friends}/>
     <TransactionHistory items={transactions} />
-    </div>  
+    </div>
+      
   )
 }
 export default App;
